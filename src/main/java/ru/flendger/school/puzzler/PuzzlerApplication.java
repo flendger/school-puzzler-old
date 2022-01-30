569edfddbdb7dbb1924a3560ba8a4501ceab136a
flendger <kiru.ee@yandex.ru>
2022-01-30 20:29:57 +0300

    initial commit

---
+++ src/main/java/ru/flendger/school/puzzler/PuzzlerApplication.java
@@ -0,0 +1,13 @@
+package ru.flendger.school.puzzler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/flendger/school/puzzler/PuzzlerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.flendger.school.puzzler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
