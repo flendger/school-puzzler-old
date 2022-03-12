ee91fcf55c1492783f86346ea5349ec8a5ccda2b
flendger <kiru.ee@yandex.ru>
2022-03-12 20:05:58 +0300

    added StudentInputParser

---
+++ src/main/java/ru/flendger/school/puzzler/model/dto/StudentImportData.java
@@ -0,0 +1,11 @@
+package ru.flendger.school.puzzler.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StudentImportData {
+    private String studentName;
+    private String schoolClassName;
+}
