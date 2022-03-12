4102470a1dfd0b768e76b3c5baef5eced46b1564
flendger <kiru.ee@yandex.ru>
2022-03-12 19:48:05 +0300

    added findByName for SchoolClassService

--- src/main/java/ru/flendger/school/puzzler/database/service/SchoolClassServiceImpl.java
+++ src/main/java/ru/flendger/school/puzzler/database/service/SchoolClassServiceImpl.java
@@ -6,13 +6,20 @@ import ru.flendger.school.puzzler.database.repositories.SchoolClassRepository;
 import ru.flendger.school.puzzler.model.entity.SchoolClass;
 import ru.flendger.school.puzzler.model.service.SchoolClassService;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class SchoolClassServiceImpl extends AbstractCrudService<SchoolClass, Long, SchoolClassRepository> implements SchoolClassService {
    private final SchoolClassRepository schoolClassRepository;
 
     @Override
-    SchoolClassRepository getRepository() {
+    protected SchoolClassRepository getRepository() {
         return schoolClassRepository;
+    }
+
+    @Override
+    public Optional<SchoolClass> findByName(String schoolClassName) {
+        return schoolClassRepository.findFirstByNameOrderByIdAsc(schoolClassName);
     }
 }
--- src/main/java/ru/flendger/school/puzzler/model/service/SchoolClassService.java
+++ src/main/java/ru/flendger/school/puzzler/model/service/SchoolClassService.java
@@ -2,5 +2,8 @@ package ru.flendger.school.puzzler.model.service;
 
 import ru.flendger.school.puzzler.model.entity.SchoolClass;
 
+import java.util.Optional;
+
 public interface SchoolClassService extends CrudService<SchoolClass, Long> {
+    Optional<SchoolClass> findByName(String schoolClassName);
 }
