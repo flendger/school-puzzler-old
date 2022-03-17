a53ce691c321bfd274b104b8922ac1046b4c3e9e
flendger <kiru.ee@yandex.ru>
2022-03-17 21:01:03 +0300

    added JwtRequest and JwtResponse

---
+++ src/main/java/ru/flendger/school/puzzler/web/dto/JwtRequest.java
@@ -0,0 +1,11 @@
+package ru.flendger.school.puzzler.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtRequest {
+    private String username;
+    private String password;
+}
