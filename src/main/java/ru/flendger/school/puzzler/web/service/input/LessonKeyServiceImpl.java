307098fd3bf56d3fd29e7994415a0a882e6ac54f
flendger <kiru.ee@yandex.ru>
2022-04-02 16:50:19 +0300

    save new LessonKey before response

--- src/main/java/ru/flendger/school/puzzler/web/service/input/LessonKeyServiceImpl.java
+++ src/main/java/ru/flendger/school/puzzler/web/service/input/LessonKeyServiceImpl.java
@@ -1,24 +1,36 @@
 package ru.flendger.school.puzzler.web.service.input;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.flendger.school.puzzler.model.service.input.DateTimeService;
 import ru.flendger.school.puzzler.web.dto.LessonKeyRequest;
 import ru.flendger.school.puzzler.web.dto.LessonKeyResponse;
+import ru.flendger.school.puzzler.web.entity.LessonKey;
 import ru.flendger.school.puzzler.web.generator.KeyGenerator;
+import ru.flendger.school.puzzler.web.service.output.LessonKeyStorageService;
 
 @Service
 @RequiredArgsConstructor
 public class LessonKeyServiceImpl implements LessonKeyService {
     private final KeyGenerator keyGenerator;
     private final DateTimeService dateTimeService;
+    private final LessonKeyStorageService lessonKeyStorageService;
+    private final ModelMapper modelMapper;
 
     @Override
+    @Transactional
     public LessonKeyResponse generateKey(LessonKeyRequest lessonKeyRequest) {
-        return new LessonKeyResponse(
+        LessonKeyResponse lessonKeyResponse = new LessonKeyResponse(
                 keyGenerator.generate(),
                 lessonKeyRequest.getLessonId(),
                 lessonKeyRequest.getSchoolClassId(),
                 dateTimeService.current());
+
+        LessonKey lessonKey = modelMapper.map(lessonKeyResponse, LessonKey.class);
+        lessonKeyStorageService.save(lessonKey);
+
+        return lessonKeyResponse;
     }
 }
