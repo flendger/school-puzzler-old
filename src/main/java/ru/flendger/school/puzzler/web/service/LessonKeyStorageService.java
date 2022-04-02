90d208164c485961c44fe1d401ecb11e59b75ee6
flendger <kiru.ee@yandex.ru>
2022-04-02 16:12:22 +0300

    added LessonKeyStorageService

---
+++ src/main/java/ru/flendger/school/puzzler/web/service/LessonKeyStorageService.java
@@ -0,0 +1,13 @@
+package ru.flendger.school.puzzler.web.service;
+
+import ru.flendger.school.puzzler.model.service.output.CrudStorageService;
+import ru.flendger.school.puzzler.web.entity.LessonKey;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface LessonKeyStorageService extends CrudStorageService<LessonKey, Long> {
+    Optional<LessonKey> findActive(String keyValue, LocalDateTime expiredDate);
+
+    void deleteExpired(LocalDateTime expiredDate);
+}
