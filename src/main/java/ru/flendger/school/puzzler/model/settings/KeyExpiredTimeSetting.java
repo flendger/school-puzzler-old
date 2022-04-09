4691aa8e5c7e6ba8feedaf08b3bc3c7b308ae654
flendger <kiru.ee@yandex.ru>
2022-04-09 14:08:25 +0300

    added KeyExpiredTimeSetting

---
+++ src/main/java/ru/flendger/school/puzzler/model/settings/KeyExpiredTimeSetting.java
@@ -0,0 +1,7 @@
+package ru.flendger.school.puzzler.model.settings;
+
+public class KeyExpiredTimeSetting extends AbstractApplicationSetting<Integer> {
+    public KeyExpiredTimeSetting() {
+        super(ApplicationSettingsKey.KEY_EXPIRED_TIME);
+    }
+}
