dbf94d45f97a3e9739ac34921250de0160b9e1a8
flendger <kiru.ee@yandex.ru>
2022-04-09 14:41:01 +0300

    added ApplicationSettingEntityStorageService

---
+++ src/main/java/ru/flendger/school/puzzler/model/service/output/ApplicationSettingEntityStorageService.java
@@ -0,0 +1,7 @@
+package ru.flendger.school.puzzler.model.service.output;
+
+import ru.flendger.school.puzzler.model.settings.ApplicationSettingEntity;
+import ru.flendger.school.puzzler.model.settings.ApplicationSettingsKey;
+
+public interface ApplicationSettingEntityStorageService extends CrudStorageService<ApplicationSettingEntity, ApplicationSettingsKey>{
+}
