a0c847133a8813db87b087f6ca483f6ddfd96d95
flendger <kiru.ee@yandex.ru>
2022-04-09 14:40:32 +0300

    added ApplicationSettingRepository

---
+++ src/main/java/ru/flendger/school/puzzler/database/repositories/ApplicationSettingRepository.java
@@ -0,0 +1,8 @@
+package ru.flendger.school.puzzler.database.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.flendger.school.puzzler.model.settings.ApplicationSettingEntity;
+import ru.flendger.school.puzzler.model.settings.ApplicationSettingsKey;
+
+public interface ApplicationSettingRepository extends JpaRepository<ApplicationSettingEntity, ApplicationSettingsKey> {
+}
---
+++ src/test/java/ru/flendger/school/puzzler/database/repositories/ApplicationSettingRepositoryTest.java
@@ -0,0 +1,34 @@
+package ru.flendger.school.puzzler.database.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import ru.flendger.school.puzzler.model.settings.AppSetting;
+import ru.flendger.school.puzzler.model.settings.ApplicationSettingEntity;
+import ru.flendger.school.puzzler.model.settings.ApplicationSettingsKey;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("off-liquibase")
+class ApplicationSettingRepositoryTest {
+    @Autowired
+    private ApplicationSettingRepository repository;
+
+    @Test
+    public void saveAndUpdateSettingsTest() throws Exception {
+        AppSetting<?> appSetting = ApplicationSettingsKey.KEY_EXPIRED_TIME.create();
+        appSetting.setStringValue("5");
+
+        ApplicationSettingEntity applicationSettingEntity = ApplicationSettingsKey.toEntity(appSetting);
+        repository.saveAndFlush(applicationSettingEntity);
+
+        appSetting.setStringValue("6");
+        ApplicationSettingEntity updatedSetting = ApplicationSettingsKey.toEntity(appSetting);
+        repository.saveAndFlush(updatedSetting);
+
+        assertEquals("6", repository.findAll().get(0).getValue());
+    }
+}
