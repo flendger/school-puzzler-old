34d4c830122ee6e78bcecc8a61eecc8f2cdd59d1
flendger <kiru.ee@yandex.ru>
2022-05-07 19:49:27 +0300

    added LessonController

---
+++ src/main/java/ru/flendger/school/puzzler/web/controller/LessonController.java
@@ -0,0 +1,21 @@
+package ru.flendger.school.puzzler.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.flendger.school.puzzler.model.service.input.LessonService;
+
+@RestController
+@RequestMapping("/api/v1/admin/lessons")
+@RequiredArgsConstructor
+public class LessonController {
+    private final LessonService lessonService;
+
+    @GetMapping
+    public ResponseEntity<?> find(@RequestParam(value = "subjectId") Long subjectId) {
+        return ResponseEntity.ok(lessonService.find(subjectId));
+    }
+}
