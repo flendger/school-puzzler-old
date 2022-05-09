cd502d0c4dd21a019ae2396f927a664a10c2a6e2
flendger <kiru.ee@yandex.ru>
2022-05-09 19:51:06 +0300

    added get students by key request

--- puzzler-app/src/components/lesson_login/LessonLogin.js
+++ puzzler-app/src/components/lesson_login/LessonLogin.js
@@ -1,24 +1,40 @@
 import {Button, Card, Form} from "react-bootstrap";
+import {useState} from "react";
+import {getStudents} from "./LessonLoginDataFunctions";
 
 export function LessonLogin() {
+    const [students, setStudents] = useState([]);
+
+    function onKeyValueChanged(e) {
+        const keyValue = e.target.value;
+        getStudents(keyValue, setStudents);
+    }
+
     return <Card
         style={{width: '18rem'}}
         className="mb-2 position-absolute top-50 start-50 translate-middle"
     >
         <Card.Header><b>Доступ к уроку</b></Card.Header>
         <Card.Body>
-            <Form>
+            <Form onSubmit={(event) => event.preventDefault()}>
                 <Form.Group className="mb-3" controlId="formLessonKey">
                     <Form.Label>Ключ</Form.Label>
-                    <Form.Control size="sm" type="text" placeholder="Введите ключ урока"/>
+                    <Form.Control
+                        size="sm"
+                        type="text"
+                        placeholder="Введите ключ урока"
+                        onBlur={onKeyValueChanged}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formLessonKeyStudent">
                     <Form.Label>Студент</Form.Label>
-                    <Form.Select size="sm" onChange={(e) => console.log(e.target.value)}>
+                    <Form.Select
+                        size="sm"
+                        onChange={(e) => console.log(e.target.value)}>
                         <option value={0}>Выберите студента</option>
-                        {/*{schoolClasses.map((schoolClass) => {*/}
-                        {/*    return <option key={schoolClass.id} value={schoolClass.id}>{schoolClass.name}</option>*/}
-                        {/*})}*/}
+                        {students.map((student) => {
+                            return <option key={student.id} value={student.id}>{student.name}</option>
+                        })}
                     </Form.Select>
                 </Form.Group>
                 <div className="text-center">
---
+++ puzzler-app/src/components/lesson_login/LessonLoginDataFunctions.js
@@ -0,0 +1,82 @@
+import axios from "axios";
+import {getConfigWithHeader, getRequestHeader} from "../../tokenUtils";
+
+const ctxPath = '/puzzler/api/v1/students/login';
+
+export function getStudents(keyValue, onSuccess) {
+    axios.get(ctxPath + "/" + keyValue, getRequestHeader())
+        .then((response) => {
+            onSuccess(response.data);
+        })
+        .catch((error) => {
+            let errMsg = error.response.data.message;
+            errMsg = errMsg ? errMsg : error;
+
+            alert(errMsg);
+        });
+}
+
+// export function getSubjects(onSuccess) {
+//     axios.get(ctxPath + "/subjects", getRequestHeader())
+//         .then((response) => {
+//             onSuccess(response.data);
+//         })
+//         .catch((error) => {
+//             let errMsg = error.response.data.message;
+//             errMsg = errMsg ? errMsg : error;
+//
+//             alert(errMsg);
+//         });
+// }
+//
+// export function getLessons(subjectId, onSuccess) {
+//     const params = {subjectId: subjectId};
+//     const configWithHeader = getConfigWithHeader({params: params});
+//
+//     axios.get(ctxPath + "/lessons", configWithHeader)
+//         .then((response) => {
+//             onSuccess(response.data);
+//         })
+//         .catch((error) => {
+//             let errMsg = error.response.data.message;
+//             errMsg = errMsg ? errMsg : error;
+//
+//             alert(errMsg);
+//         });
+// }
+//
+// export function getClasses(onSuccess) {
+//     axios.get(ctxPath + "/classes", getRequestHeader())
+//         .then((response) => {
+//             onSuccess(response.data);
+//         })
+//         .catch((error) => {
+//             let errMsg = error.response.data.message;
+//             errMsg = errMsg ? errMsg : error;
+//
+//             alert(errMsg);
+//         });
+// }
+//
+// export function generateKey(keyRequest, onSuccess) {
+//     axios.post(ctxPath + "/lkey", keyRequest, getRequestHeader())
+//         .then((response) => {
+//             onSuccess(response.data);
+//         })
+//         .catch(error => {
+//             alert(error.response.data.message);
+//         });
+// }
+//
+// export function deleteKey(keyValue, onSuccess) {
+//     axios.delete(ctxPath + "/lkey/" + keyValue, getRequestHeader())
+//         .then(() => {
+//             onSuccess(keyValue);
+//         })
+//         .catch((error) => {
+//             let errMsg = error.response.data.message;
+//             errMsg = errMsg ? errMsg : error;
+//
+//             alert(errMsg);
+//         });
+// }
