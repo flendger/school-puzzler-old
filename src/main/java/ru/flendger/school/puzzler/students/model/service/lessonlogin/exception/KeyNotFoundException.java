2b1f6986376954e1fb99d007b5ae852e8f28ddf4
flendger <kiru.ee@yandex.ru>
2022-06-12 22:25:30 +0300

    added KeyNotFoundException

---
+++ src/main/java/ru/flendger/school/puzzler/students/model/service/lessonlogin/exception/KeyNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.flendger.school.puzzler.students.model.service.lessonlogin.exception;
+
+public class KeyNotFoundException extends RuntimeException{
+    public KeyNotFoundException(String message) {
+        super(message);
+    }
+}
