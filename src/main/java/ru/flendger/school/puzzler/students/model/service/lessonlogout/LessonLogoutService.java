39c13d87123e849657ba54e6f784e8de26c0828a
flendger <kiru.ee@yandex.ru>
2022-06-14 21:59:01 +0300

    added StudentLogoutController

---
+++ src/main/java/ru/flendger/school/puzzler/students/model/service/lessonlogout/LessonLogoutService.java
@@ -0,0 +1,5 @@
+package ru.flendger.school.puzzler.students.model.service.lessonlogout;
+
+public interface LessonLogoutService {
+    void logout(Long studentId);
+}
---
+++ src/main/java/ru/flendger/school/puzzler/web/controller/students/StudentLogoutController.java
@@ -0,0 +1,38 @@
+package ru.flendger.school.puzzler.web.controller.students;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.flendger.school.puzzler.students.model.service.lessonlogout.LessonLogoutService;
+import ru.flendger.school.puzzler.web.dto.message.ResponseMessage;
+
+import java.security.Principal;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/api/v1/students/logout")
+@RequiredArgsConstructor
+@Slf4j
+public class StudentLogoutController {
+    private final LessonLogoutService lessonLogoutService;
+
+    @PostMapping
+    public ResponseEntity<?> logout(Principal principal) {
+        if (Objects.isNull(principal)) {
+            return ResponseMessage.createResponse("Студент не авторизован", HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            lessonLogoutService.logout(Long.parseLong(principal.getName()));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseMessage.createResponse("Не удалось выйти из урока", HttpStatus.BAD_REQUEST);
+        }
+
+        return ResponseEntity.ok().build();
+    }
+}
