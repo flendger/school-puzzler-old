6e91e38a9af654179214aca746bb96070f6c5df9
flendger <kiru.ee@yandex.ru>
2022-06-16 22:36:52 +0300

    getActive StudentLessonKeyRowDtos

---
+++ src/main/java/ru/flendger/school/puzzler/students/model/service/studentslessonkeys/StudentsLessonKeysServiceImpl.java
@@ -0,0 +1,53 @@
+package ru.flendger.school.puzzler.students.model.service.studentslessonkeys;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import ru.flendger.school.puzzler.lessons.model.service.lesson.LessonService;
+import ru.flendger.school.puzzler.lessons.model.service.lesson.dto.LessonRowDto;
+import ru.flendger.school.puzzler.students.model.dao.StudentLessonKeyStorageService;
+import ru.flendger.school.puzzler.students.model.dao.StudentLessonStorageService;
+import ru.flendger.school.puzzler.students.model.entity.StudentLesson;
+import ru.flendger.school.puzzler.students.model.service.studentslessonkeys.dto.StudentLessonKeyRowDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class StudentsLessonKeysServiceImpl implements StudentsLessonKeysService {
+    private final StudentLessonKeyStorageService studentLessonKeyStorageService;
+    private final ModelMapper studentsModelMapper;
+    private final StudentLessonStorageService studentLessonStorageService;
+    private final LessonService lessonService;
+
+    @Override
+    public List<StudentLessonKeyRowDto> getActive() {
+        return studentLessonKeyStorageService.findActive()
+                .stream()
+                .map(studentLessonKey -> {
+                    StudentLessonKeyRowDto studentLessonKeyRowDto = studentsModelMapper.map(studentLessonKey, StudentLessonKeyRowDto.class);
+
+                    studentLessonStorageService
+                            .findById(studentLessonKeyRowDto.getStudentLessonId())
+                            .ifPresent(studentLesson -> {
+                                studentLessonKeyRowDto.setStudentName(getStudentName(studentLesson));
+
+                                lessonService
+                                        .findRowById(studentLesson.getLessonId())
+                                        .ifPresent(lessonRowDto -> studentLessonKeyRowDto.setLessonName(getLessonName(lessonRowDto)));
+                            });
+
+                    return studentLessonKeyRowDto;
+                })
+                .collect(Collectors.toList());
+    }
+
+    private String getLessonName(LessonRowDto lessonRowDto) {
+        return lessonRowDto.getName();
+    }
+
+    private String getStudentName(StudentLesson studentLesson) {
+        return studentLesson.getStudent().getName();
+    }
+}
--- src/main/java/ru/flendger/school/puzzler/students/model/service/studentslessonkeys/dto/StudentLessonKeyRowDto.java
+++ src/main/java/ru/flendger/school/puzzler/students/model/service/studentslessonkeys/dto/StudentLessonKeyRowDto.java
@@ -1,4 +1,18 @@
 package ru.flendger.school.puzzler.students.model.service.studentslessonkeys.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class StudentLessonKeyRowDto {
+    private Long id;
+    private String keyValue;
+    private LocalDateTime loginDate;
+    private Long studentLessonId;
+
+    private String studentName = "";
+    private String lessonName = "";
 }
